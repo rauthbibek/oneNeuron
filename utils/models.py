7efda06d0cdf63697e5763c5905c53f6aa2546ef
bibek.rauth <bibek.rauth@i-exceed.com>
2021-09-21 23:38:04 +0530

    tqdm module used for progress bar

--- utils/models.py
+++ utils/models.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import logging
+from tqdm import tqdm
 
 class Perceptron:
 
@@ -22,7 +23,7 @@ class Perceptron:
     X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] # CONCATINATION
     logging.info(f"X with bias: \n{X_with_bias}")
 
-    for epoch in range(self.epochs):
+    for epoch in tqdm(range(self.epochs), total=self.epochs, desc="training the model"):
       logging.info("--"*10)
       logging.info(f"for epoch: {epoch}")
       logging.info("--"*10)
